637daa938ecd10f6d5e3531f4586cd206165f67d
arupjbasu <arupjbasu@gmail.com>
2019-10-13 13:00:26 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 import { Observable, Observer, Subscription, fromEvent } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { Http } from '@angular/http';
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
@@ -12,6 +14,9 @@ export class AppComponent  {
   observable: Observable<string>;
   observer: Observer<string>;
   subscription: Subscription;
+
+  constructor(private http: Http) { }
+
   ngOnInit() {
   this.observable = new Observable((observer: Observer<string>) => {
     this.observer = observer;
@@ -24,8 +29,20 @@ export class AppComponent  {
     this.observer.next('15');
     this.observer.complete();
     this.observer.next('16');
-}
 
+    this.http.get('https://jsonplaceholder.typicode.com/posts')
+    .pipe(map(this.extractData))
+    .subscribe(this.handleData, this.handleError, this.handleComplete);
+
+    this.subscription = fromEvent(document, 'mousemove')
+    .subscribe(this.handleData, this.handleError, this.handleComplete);
+}
+extractData(response) {
+  console.log('Raw response:', response);
+  console.log('Formatted response:', response.json());
+  const body = response.json();
+  return body || { };
+}
 handleData(data) {
     console.log('Here are the usable data', data);
     // Insert Business logic here
